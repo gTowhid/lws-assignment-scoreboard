d2a34abe8f4e7b5f964aa288c9571761893053f4
Towhid <towhidgolam8182@gmail.com>
2023-02-19 00:01:39 +0600

    Redux scoreboard first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// initial state
+const initialState = {
+  value: 0,
+};
+
+// create reducer function
+function counterReducer(state = initialState, action) {
+  if (action.type === 'increment') {
+    return {
+      ...state,
+      value: state.value + 1,
+    };
+  } else if (action.type === 'decrement') {
+    return {
+      ...state,
+      value: state.value - 1,
+    };
+  }
+}
